e53b1587894d5567389b94777399892baa236df7
Kushuvardan <132328167+Kushuvardan@users.noreply.github.com>
2023-05-02 14:35:35 +0530

    Create Question 20.C

---
+++ Question 20.C
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main() {
+
+   int num;
+
+   printf("Enter an integer number: ");
+
+   scanf("%d", &num);
+
+   if (num % 2 == 0) {
+
+      printf("%d is an even number.\n", num);
+
+   } else {
+
+      printf("%d is an odd number.\n", num);
+
+   }
+
+   return 0;
+
+}
